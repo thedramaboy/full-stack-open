f33621edb8ed618f49e44e2aa9cbd3e4022c541f
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2025-12-29 14:18:28 -0700

    Exercises 1.1-1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,49 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  const total = exercises1 + exercises2 + exercises3;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Total total={total} />
+    </div>
+  );
+};
+
+export default App;
