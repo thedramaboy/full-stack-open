429765df44763ac3b9151fc160a792f2b4dea96d
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-01-01 16:30:09 -0700

    Exercises 1.6-1.11 unicafe

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+const Header = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <StatisticLine text="good" value={good} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine text="neutral" value={neutral} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine text="bad" value={bad} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine text="all" value={total} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine text="average" value={average} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <StatisticLine text="positive" value={positive} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <Header text="give feedback" />
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Header text="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
