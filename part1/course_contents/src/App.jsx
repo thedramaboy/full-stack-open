0a7abefdd56656748469c32c91e3a2d23a80f84e
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-01-01 16:29:16 -0700

    Finished course contents

--- part1/course_contents/src/App.jsx
+++ part1/course_contents/src/App.jsx
@@ -1,22 +1,78 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  );
+// import { useState } from "react";
+
+import { useState } from "react";
+
+// const Display = ({counter}) => {
+//   return <div>{counter}</div>;
+// };
+
+// const Button = ({onClick, text}) => {
+//   return <button onClick={onClick}>{text}</button>;
+// };
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
+//   console.log("rendering with counter value", counter);
+
+//   const increaseByOne = () => {
+//     console.log("increasing, value before", counter);
+//     setCounter(counter + 1);
+//   };
+
+//   const decreaseByOne = () => {
+//     console.log("decreasing, value before", counter);
+//     setCounter(counter - 1);
+//   };
+//   const setToZero = () => {
+//     console.log("resetting to zero, value before", counter);
+//     setCounter(0);
+//   };
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={increaseByOne} text="Plus" />
+//       <Button onClick={setToZero} text="Zero" />
+//       <Button onClick={decreaseByOne} text="Minus" />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
 };
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
-      <h1>Greetings!</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      {left}
+
+      <Button onClick={handleLeftClick} text="left" />
+      <Button onClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
