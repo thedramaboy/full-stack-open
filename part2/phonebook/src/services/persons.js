9c64c02c4c108c235411f323f73d6f63d600da37
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-02-19 16:11:49 -0700

    feat/ production build for exercises 3.9-3.11

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "https://fso-backend.vercel.app/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
@@ -14,8 +14,8 @@ const create = (newObject) => {
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data)
-}
+  return request.then((response) => response.data);
+};
 
 const deletePerson = (id) => axios.delete(`${baseUrl}/${id}`);
 
