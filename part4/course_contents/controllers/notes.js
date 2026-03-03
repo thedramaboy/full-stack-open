0bc13a5a0bf26fc8527baa79e0c6debd329f2cfd
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-03-03 15:42:54 -0700

    feat/ finished part4 course contents

--- part4/course_contents/controllers/notes.js
+++ part4/course_contents/controllers/notes.js
@@ -1,9 +1,19 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/notes");
 const User = require("../models/users");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
+
   response.json(notes);
 });
 
@@ -18,8 +28,12 @@ notesRouter.get("/:id", async (request, response) => {
 
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
 
-  const user = await User.findById(body.userId);
+  const user = await User.findById(decodedToken.id);
 
   if (!user) {
     return response.status(400).json({ error: "userId missing or not valid" });
--- part4/course_contents/controllers/users.js
+++ part4/course_contents/controllers/users.js
@@ -2,10 +2,14 @@ const usersRouter = require("express").Router();
 const User = require("../models/users");
 const bcrypt = require("bcrypt");
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
-})
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("notes", {
+    content: 1,
+    important: 1,
+  });
+
+  response.json(users);
+});
 
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
