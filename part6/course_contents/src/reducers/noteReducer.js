672946ebc80ef7439f5f193acb443fefd1f7bd38
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-03-22 15:36:53 -0600

    feat/ exercises 6.3-6.5

--- part6/course_contents/src/reducers/noteReducer.js
+++ part6/course_contents/src/reducers/noteReducer.js
@@ -1,8 +1,7 @@
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case "NEW_NOTE":
-      return state.concat(action.payload);
-
+      return [...state, action.payload];
     case "TOGGLE_IMPORTANCE": {
       const id = action.payload.id;
       const noteToChange = state.find((n) => n.id === id);
@@ -15,6 +14,26 @@ const noteReducer = (state = [], action) => {
     default:
       return state;
   }
+};
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+export const createNote = (content) => {
+  return {
+    type: "NEW_NOTE",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: { id },
+  };
 };
 
 export default noteReducer;
