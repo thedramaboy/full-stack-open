f7de46eb4a2a87803410d562c46342c9422336e0
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2026-04-01 21:08:24 -0600

    feat/ exercises 6.20-6.22

---
+++ part6/query-anecdotes/src/requests.js
@@ -0,0 +1,40 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = async () => {
+  const response = await fetch(baseUrl)
+  if (!response.ok) {
+    throw new Error('Failed to fetch anecdotes')
+  }
+  return await response.json()
+}
+
+export const createAnecdote = async (newAnecdote) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newAnecdote),
+  }
+
+  const response = await fetch(baseUrl, options)
+
+  if (!response.ok) {
+    throw new Error('Failed to create anecdote')
+  }
+  return await response.json()
+}
+
+export const updateVote = async (updatedAnecdote) => {
+  const options = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(updatedAnecdote),
+  }
+
+  const response = await fetch(`${baseUrl}/${updatedAnecdote.id}`, options)
+
+  if (!response.ok) {
+    throw new Error('Failed to vote anecdote')
+  }
+
+  return await response.json()
+}
